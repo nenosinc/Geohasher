3768e3102edd2f43b0f337aa1c05c0c8c1a4f918
Sam Spencer <samspencer@hey.com>
2021-07-21 12:35:59 -0500

    Added CLI

--- Package.swift
+++ Package.swift
@@ -12,18 +12,20 @@ let package = Package(
         .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them
+        // visible to other packages.
         .library(
             name: "Geohasher",
-            targets: ["Geohasher"]),
+            targets: ["Geohasher"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "7.3.0")
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets are the basic building blocks of a package. A target can define a module
+        // or a test suite. Targets can depend on other targets in this package, and on
+        // products in packages this package depends on.
         .target(
             name: "Geohasher",
             dependencies: [
